8062c74b2242da37c8b9560e5022e0aedab5cf7e
Nathan Neff <nathan.neff@gmail.com>
2012-01-03 12:10:33 -0600

    Another empty vector test

--- test/avg/test/core.clj
+++ test/avg/test/core.clj
@@ -6,5 +6,5 @@
   (testing "when-three-items-and-index-one-return-us"
     (is (= [1 2 3] (c/neighborhood [1 2 3] 1))))
   (testing "when-empty-vector-passed-empty-vector-returned"
-    (is (= [] (c/neighborhood [] 0))))  
+    (is (= [] (c/neighborhood [] 55))))  
 )
